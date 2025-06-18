a134a6843e9dfee5f02d8a357476c38b47a41f74
Bullet2004 <buithecuong2004@gmail.com>
2025-06-18 17:05:24 +0700

    feat: add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Pages/Home/Home'
+import Video from './Pages/Video/Video'
 
 function App() {
-
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/video/:categoryId/:videoId' element={<Video />} />
+      </Routes>
     </>
   )
 }
